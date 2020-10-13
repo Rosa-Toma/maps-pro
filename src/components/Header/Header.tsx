94a17aae48b89e40978b670a8b3323b89fb50e2b
Toma Rosa <rosa.toma@outlook.fr>
2020-10-13 12:02:18 +0200

    Toolbar setup et mise en place des intéraction avec la map (zoom/dézoom)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,34 @@ import React from "react";
 
 import styles from "./Header.module.scss";
 
+import logoIread from "../../assets/logo.png";
+
 export const Header = () => {
-    return <div className={styles.header}></div>;
+    return (
+        <div className={styles.header}>
+            <Logo imgSrc={logoIread} />
+            <TabSection>
+                <Tab title={"tab"} />
+                <Tab title={"tab"} />
+                <Tab title={"tab"} />
+                <Tab title={"tab"} />
+            </TabSection>
+        </div>
+    );
+};
+
+export const Logo = ({ imgSrc }: any) => {
+    return <img className={styles.logo} src={imgSrc}></img>;
+};
+
+export const TabSection = ({ children }: any) => {
+    return <div className={styles.tabSection}>{children}</div>;
+};
+
+export const Tab = ({ title }: any) => {
+    return (
+        <button className={styles.tab}>
+            <h1 className={styles.tab__title}>{title}</h1>
+        </button>
+    );
 };
